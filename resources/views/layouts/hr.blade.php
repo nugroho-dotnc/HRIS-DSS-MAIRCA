c749350cd03ef7fa77c4125f2da94767e9d5ff66
= <=>
2026-05-20 23:49:37 +0700

    setting layout HR

---
+++ resources/views/layouts/hr.blade.php
@@ -0,0 +1,8 @@
+@props(['page_title' => null, 'page_description' => null, 'title' => null])
+<x-layouts::app.hr-sidebar :title="$title ?? null">
+    <flux:main class="h-screen overflow-y-auto">
+        <flux:heading size="xl">{{$page_title??null}}</flux:heading>
+        <flux:subheading>{{$page_description??null}}</flux:subheading>
+        {{ $slot }}
+    </flux:main>
+</x-layouts::app.hr-sidebar>
---
+++ resources/views/pages/hr/⚡dashboard.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+
+new #[Layout('layouts::hr', ['page_title' => 'Dashboard'])] class extends Component
+{
+    //
+};
+?>
+
+<div>
+    {{-- Simplicity is an acquired taste. - Katharine Gerould --}}
+</div>
