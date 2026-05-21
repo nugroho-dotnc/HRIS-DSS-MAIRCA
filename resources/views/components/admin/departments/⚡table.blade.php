3456fc9d00d6326e0e4bde7682efe7e7d45f35b3
= <=>
2026-05-21 12:08:57 +0700

    department table adjustment

--- resources/views/components/admin/departments/⚡table.blade.php
+++ resources/views/components/admin/departments/⚡table.blade.php
@@ -59,42 +59,42 @@ new class extends Component
 ?>
 
 <div>
-    {{-- Do what you can, with what you have, where you are. - Theodore Roosevelt --}}
-    <div class="flex justify-between items-center  mt-5">
-        <flux:input wire:model.live.debounce.300ms="search" class="w-full max-w-md" kbd="⌘K" icon="magnifying-glass" placeholder="Search..."/>
-        <flux:modal.trigger name="add-department">
-            <flux:button class="cursor-pointer">Tambah</flux:button>
-        </flux:modal.trigger>
+    <div class="flex flex-1 flex-col gap-4 rounded-xl">
+        <div class="flex justify-between items-center">
+            <flux:input wire:model.live.debounce.300ms="search" class="w-full max-w-md" kbd="⌘K" icon="magnifying-glass" placeholder="Search..."/>
+            <flux:modal.trigger name="add-department">
+                <flux:button class="cursor-pointer">Tambah</flux:button>
+            </flux:modal.trigger>
+        </div>
+
+        <flux:table :paginate="$this->departments()">
+            <flux:table.columns>
+                <flux:table.column class="w-1/3">Department Name</flux:table.column>
+                <flux:table.column class="w-1/3">Status</flux:table.column>
+                <flux:table.column class="w-1/3">Action</flux:table.column>
+            </flux:table.columns>
+
+            <flux:table.rows>
+                @foreach ($this->departments() as $department)
+                    <flux:table.row wire:key="department-{{ $department->id }}-{{ $department->is_active }}">
+                        <flux:table.cell class="flex items-center gap-3">
+                            {{ $department->department_name }}
+                        </flux:table.cell>
+                        <flux:table.cell>
+                            <flux:badge color="{{$department->is_active?'green':'red'}}" size="sm">
+                                {{$department->is_active?'active':'inactive'}}
+                            </flux:badge>
+                        </flux:table.cell>
+                        <flux:table.cell class="space-x-4">
+                            <flux:switch :checked="$department->is_active" wire:click="isActiveToggle({{ $department->id }})"/>
+                            <flux:button icon="trash" size="sm" type="button" variant="danger" wire:click="delete({{$department->id}})" wire:confirm="Are you sure you want to delete this post?"></flux:button>
+                            <flux:button class="cursor-pointer" size="sm" icon="pencil" wire:click="openEdit({{$department->id}})"/>
+                        </flux:table.cell>
+                    </flux:table.row>
+                @endforeach
+            </flux:table.rows>
+        </flux:table>
     </div>
-    <flux:spacer />
-
-    <flux:table class="mt-5" :paginate="$this->departments()">
-        <flux:table.columns>
-            <flux:table.column class="w-1/3">Department Name</flux:table.column>
-            <flux:table.column class="w-1/3">Status</flux:table.column>
-            <flux:table.column class="w-1/3">Action</flux:table.column>
-        </flux:table.columns>
-
-        <flux:table.rows>
-            @foreach ($this->departments() as $department)
-                <flux:table.row wire:key="department-{{ $department->id }}-{{ $department->is_active }}">
-                    <flux:table.cell class="flex items-center gap-3">
-                        {{ $department->department_name }}
-                    </flux:table.cell>
-                    <flux:table.cell>
-                        <flux:badge color="{{$department->is_active?'green':'red'}}">
-                            {{$department->is_active?'active':'inactive'}}
-                        </flux:badge>
-                    </flux:table.cell>
-                    <flux:table.cell class="space-x-4">
-                        <flux:switch :checked="$department->is_active" wire:click="isActiveToggle({{ $department->id }})"/>
-                        <flux:button icon="trash" size="sm" type="button" variant="danger" wire:click="delete({{$department->id}})" wire:confirm="Are you sure you want to delete this post?"></flux:button>
-                        <flux:button class="cursor-pointer" size="sm" icon="pencil" wire:click="openEdit({{$department->id}})"/>
-                    </flux:table.cell>
-                </flux:table.row>
-            @endforeach
-        </flux:table.rows>
-    </flux:table>
     <flux:modal name="add-department" class="md:w-4xl">
         @livewire('admin.departments.add-form')
     </flux:modal>
