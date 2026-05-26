5606b96bdfc20efb576f0f00ed88ea9807f82f07
= <=>
2026-05-26 12:22:30 +0700

    feat: make admin dashboard page

--- resources/views/pages/admin/⚡dashboard.blade.php
+++ resources/views/pages/admin/⚡dashboard.blade.php
@@ -3,12 +3,24 @@
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 
-new #[Layout('layouts::admin', ['page_title' => 'Dashboard'])] class extends Component
+new #[Layout('layouts::admin', ['page_title' => 'Dashboard', 'page_description' => 'Ringkasan data master, user, posisi, dan kesiapan kriteria DSS'])] class extends Component
 {
     //
 };
 ?>
 
-<div>
-    {{-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius --}}
+<div class="flex flex-1 flex-col gap-6">
+    <livewire:admin.dashboard.header />
+
+    <livewire:admin.dashboard.stats-overview />
+
+    <div class="grid grid-cols-1 gap-6 xl:grid-cols-2">
+        <livewire:admin.dashboard.role-distribution />
+        <livewire:admin.dashboard.master-data-health />
+    </div>
+
+    <div class="grid grid-cols-1 gap-6 xl:grid-cols-2">
+        <livewire:admin.dashboard.latest-users />
+        <livewire:admin.dashboard.positions-criteria />
+    </div>
 </div>
